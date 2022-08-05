bae8ae752442e0c7cd8488583ffb2ce5484f8cb7
Diogenes Monteiro <dmonteiro@deloitte.co.uk>
2022-08-05 08:51:53 +0100

    Add project files.

---
+++ StudentEnrolment/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentEnrolment
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var dbContext = new StudentEnrolmentDbContext();
+
+            DbBootStrap.DbCheck(dbContext);
+
+            var students = dbContext.Students;
+            var courses = dbContext.Courses;
+            var subjects = dbContext.Subjects;
+            var courseSubjects = dbContext.CourseSubjects;
+            var courseMemberships = dbContext.CourseMemberships;
+
+            do
+            {
+                Console.WriteLine("\n=====================================" +
+                                  "\n   Student's Enrolment Management" +
+                                  "\n=====================================" +
+                                  "\nStudents" +
+                                  "\n  1) List     2) Add      3) Delete" +
+                                  "\n ------------------------------------" +
+                                  "\nCourses" +
+                                  "\n  4) List     5) Add      6) Delete" +
+                                  "\n ------------------------------------" +
+                                  "\nSubjects" +
+                                  "\n  7) List     8) Add      9) Delete" +
+                                  "\n ------------------------------------" +
+                                  "\n 10) Enroll student in a course" +
+                                  "\n 11) Cancel student enrolment" +
+                                  "\n 12) Student's enrolment course check" +
+                                  "\n ------------------------------------" +
+                                  "\n  0) Exit application" +
+                                  "\n ------------------------------------\n");
+
+                Console.WriteLine("Type your option or press ENTER to exit:");
+                var option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1":
+                        StudentsManagement.ListAllStudents(students);
+                        break;
+                    case "2":
+                        StudentsManagement.AddStudent(students);
+                        break;
+                    case "3":
+                        StudentsManagement.DeleteStudent(students);
+                        break;
+                    case "4":
+                        CourseManagement.ListAllCourses(courses);
+                        break;
+                    case "5":
+                        CourseManagement.AddCourse(courses);
+                        break;
+                    case "6":
+                        CourseManagement.DeleteCourse(courses);
+                        break;
+                    case "7":
+                        SubjectManagement.ListAllSubjects(subjects);
+                        break;
+                    case "8":
+                        SubjectManagement.AddSubject(subjects, courseSubjects);
+                        break;
+                    case "9":
+                        SubjectManagement.DeleteSubject(subjects, courseSubjects);
+                        break;
+                    case "10":
+                        MembershipManagement.EnrollStudent(students, courseMemberships);
+                        break;
+                    case "11":
+                        MembershipManagement.CancelStudentEnrolment(students, courseMemberships);
+                        break;
+                    case "12":
+                        MembershipManagement.StudentCourseEnrolled(students, courseMemberships);
+                        break;
+                    case "0":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        continue;
+                }
+                dbContext.SaveChanges();
+            } while (true);
+        }
+    }
+}
---
+++ StudentEnrolment/StudentsManagement.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace StudentEnrolment
+{
+    internal static class StudentsManagement
+    {
+        public static void ListAllStudents(DbSet<Student> students)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\nID\tName");
+            Console.ResetColor();
+
+            foreach (var student in students)
+            {
+                Console.WriteLine("{0}\t{1} {2}", student.Id, student.FirstName, student.LastName);
+            }
+        }
+
+        public static void AddStudent(DbSet<Student> students)
+        {
+            var student = new Student();
+            Console.WriteLine("Student first name: ");
+            student.FirstName = Console.ReadLine();
+            Console.WriteLine("Student last name: ");
+            student.LastName = Console.ReadLine();
+
+            students.Add(student);
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Student added.");
+            Console.ResetColor();
+        }
+
+        public static void DeleteStudent(DbSet<Student> students)
+        {
+            Console.WriteLine("Student ID to delete: ");
+            var studentId = Convert.ToInt32(Console.ReadLine());
+            var student = students.Find(studentId);
+
+            students.Remove(student);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Student with ID {0} has been removed", studentId);
+            Console.ResetColor();
+        }
+    }
+}
